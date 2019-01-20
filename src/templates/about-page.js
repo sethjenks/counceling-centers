349eb8216ae1f25e213dd6e7d5aeb5742fdee590
Seth Jenks <sethjenks@gmail.com>
2019-01-19 22:45:02 -0700

    Changed the about page and some alt tags on images

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -19,15 +19,14 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
           <div className="column is-4">
             <div className="section">
               <figure className="image">
-                <img className="is-rounded" src={perriann}/>
+                <img className="is-rounded" src={perriann} alt="Perriann Beyers"/>
               </figure>
             </div>
           </div>
           <div className="column is-8">
             <div className="section">
               <h3 className="title is-size-4 has-text-weight-bold is-bold-light">Perriann Parks Beyers, LCSW, MFT, CSAT I</h3>
-              <p>is a Licensed Clinical Social Worker  (LCSW), Marriage and Family Therapist (MFT), and a Certified Sexual Addiction Therapist (CSAT I).  She has over 25 years in clinical experience, and worked full time at LDS Family Services (LDSFS) for over 14 years. Her clinical experience includes working with abuse, addictions, co-dependency, singles counseling, and marriage & family therapy.  She is also a Certified  Sex Addiction Therapist and has worked with pornography addictions, as well as all types of  other addictions.  In addition, Perriann has over 17 years of expertise in group therapy for men and women clients.  She has also worked with divorce adjustment and blended family issues for adults, teens and children, and has facilitated AMAC (Adults Molested as Children) Groups. She also is a Gottman Certified Educator and is Certified as an EMDR (Eye Movement Desensitization Reprocessing) Trained Therapist.
-                Perriann is Founder and Executive Director of Counseling Centers of Utah. Direct...801-558-7054 Email: ppb.ccu@gmail.com
+              <p>is a Licensed Clinical Social Worker (LCSW), Marriage and Family Therapist (MFT), and a Certified Sexual Addiction Therapist (CSAT I). She has over 25 years in clinical experience, and worked full time at LDS Family Services (LDSFS) for over 14 years. Her clinical experience includes working with abuse, addictions, co-dependency, singles counseling, and marriage & family therapy. Perriann is also a Certified Sex Addiction Therapist and has worked with pornography addictions, as well as all types of other addictions. In addition, she has over 17 years of expertise in group therapy working with men and women clients, and has worked with divorce adjustment and blended family issues with adults, teens and children, as well as facilitated AMAC (Adults Molested as Children) Groups. Perriann is also a Gottman Certified Educator and Certified as an EMDR (Eye Movement Desensitization Reprocessing) Trained Therapist. She is active in giving back to the community, as she has served children as a volunteer at the Christmas Box House and teens at the Salt Lake County Crisis Residential Home, as well as serving adults in the Salt Lake Inner City. Perriann is Founder and Executive Director of Counseling Centers of Utah. Direct...801-558-7054 Email: ppb.ccu@gmail.com
               </p>
             </div>
           </div>
@@ -40,15 +39,14 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
           <div className="column is-4">
             <div className="section">
               <figure className="image">
-                <img className="is-rounded" src={steve}/>
+                <img className="is-rounded" src={steve} alt="Steve Beyers"/>
               </figure>
             </div>
           </div>
           <div className="column is-8">
             <div className="section">
               <h3 className="title is-size-4 has-text-weight-bold is-bold-light">Stephen Beyers, CMHC, MC</h3>
-              <p>is a Licensed Clinical Social Worker  (LCSW), Marriage and Family Therapist (MFT), and a Certified Sexual Addiction Therapist (CSAT I).  She has over 25 years in clinical experience, and worked full time at LDS Family Services (LDSFS) for over 14 years. Her clinical experience includes working with abuse, addictions, co-dependency, singles counseling, and marriage & family therapy.  She is also a Certified  Sex Addiction Therapist and has worked with pornography addictions, as well as all types of  other addictions.  In addition, Perriann has over 17 years of expertise in group therapy for men and women clients.  She has also worked with divorce adjustment and blended family issues for adults, teens and children, and has facilitated AMAC (Adults Molested as Children) Groups. She also is a Gottman Certified Educator and is Certified as an EMDR (Eye Movement Desensitization Reprocessing) Trained Therapist.
-                Perriann is Founder and Executive Director of Counseling Centers of Utah. Direct...801-558-7054 Email: ppb.ccu@gmail.com
+              <p>is a Clinical Mental Health Counselor (CMHC) with a Masters Degree in Mental Health Counseling (MC). As a Certified EMDR (Eye Movement Desensitization Reprocessing) Trained Therapist, as well as a DBT Trained Therapist, Steve has expertise in working with personality & mood disorders, trauma, substance abuse, troubled teens, pornography & other addictions, and single adults. He also has extensive experience in couple’s therapy, and divorce & marital therapy, as Steve is a Gottman Certified Educator. Steve has clinical experience in working with males, couples, and group therapy. He has worked in conjunction with Court Ordered Substance Abuse Programs, Court Ordered Public Indecency & Lewdness Programs, and the State of Utah Juvenile Justice Services for ecclesiastical and individual mentoring for incarcerated youth. In addition, Steve has an extensive business background in the business sector, including 40 years in banking & finance, business management and organizational behavior. He has served in the nonprofit sector, sitting on numerous Boards, including Fourth Street Clinic, and currently on the Board of Directors and the Executive Committee of the Utah Nonprofits Association. Steve is Co-Founder and Financial Director of Counseling Centers of Utah.
               </p>
             </div>
           </div>
@@ -61,15 +59,14 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
           <div className="column is-4">
             <div className="section">
               <figure className="image">
-                <img className="is-rounded" src={jackie}/>
+                <img className="is-rounded" src={jackie} alt="Jackie Rodat"/>
               </figure>
             </div>
           </div>
           <div className="column is-8">
             <div className="section">
               <h3 className="title is-size-4 has-text-weight-bold is-bold-light">Jackie Rodat, LMFT</h3>
-              <p>Jackie Rodat is a Licensed Marriage and Family Therapist in private practice at Counseling Centers of Utah. Jackie has been specializing in working with teens and adolescents for the past 20+ years. She has vast experience in this field, as she also currently sees clients at Westridge Academy Residential Treatment Center, working with youth who struggle with behavioral and mental health issues. Jackie established the Intensive Outpatient Program for adolescents through Salt Lake Behavioral Health, and worked in the Inpatient Unit, as well.  Jackie has a long history working in residential treatment settings such as Vista Adolescent Treatment Center and Alpine Academy.  Her subsequent years of training and experience also include case management with Vietnamese and Somalian refugees, individual and family therapy with Catholic Community Services, individual and group therapy with adult and adolescent sex offenders through Center for Family Development, and therapy with adolescents and their families in substance abuse treatment with Cornerstone Counseling Center.  Jackie has a variety of experience working with disorders including: Adoption/Reactive Attachment Disorders, Adjustment Disorders, Autism Spectrum Disorders, Nonverbal Learning Disorder, ADD/ADHD, Anxiety, Borderline Personality Disorder, Bipolar Disorder, Depression, Eating Disorders, Oppositional Defiant Disorder, PTSD, Self-Harm and Sexual Abuse.  Jackie has a great passion for working with children, adolescents and their families, and seeing them empower themselves as their self-esteem grows.
-              </p>
+              <p>is a Licensed Marriage and Family Therapist in private practice at Counseling Centers of Utah. Jackie has been specializing in working with teens and adolescents for the past 20+ years, who struggle with behavioral and mental health issues. Jackie worked in the Inpatient Unit through Salt Lake Behavioral Health, and established the Intensive Outpatient Program for adolescents. She has vast experience working in residential treatment settings such as Vista Adolescent Treatment Center, Alpine Academy, and currently at Westridge Academy Residential Treatment Center. Her subsequent years of training and experience also include case management with Vietnamese and Somalian refugees, individual and family therapy with Catholic Community Services, with sex offenders through Center for Family Development, and substance abuse treatment with Cornerstone Counseling Center. Jackie has a variety of experience working with disorders including: Adoption/Reactive Attachment & Adjustment Disorders, Autism Spectrum Disorders, ADD/ADHD, Personality & Mood Disorders, PTSD, Self-Harm and Sexual Abuse, and several others. Jackie’s passion is working with children, adolescents and their families.
             </div>
           </div>
         </div>
